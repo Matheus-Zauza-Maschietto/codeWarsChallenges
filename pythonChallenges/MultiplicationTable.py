1df37d8b6c92b4117b49f41617bf96de7e1de445
Matheus Zauza Maschietto <matheus_maschietto@yahoo.com.br>
2022-12-02 13:27:21 -0300

    update jogo sudoku

--- pythonChallenges/MultiplicationTable.py
+++ pythonChallenges/MultiplicationTable.py
@@ -2,7 +2,6 @@
 
 def multiplication_table(size):
     lista = [[x*y for y in range(1, size+1)] for x in range(1, size+1)]
-    
     return lista
 
-print(multiplication_table(3))
+print(multiplication_table(10))
--- pythonChallenges/ValidateSudokuWithSizeNxN.py
+++ pythonChallenges/ValidateSudokuWithSizeNxN.py
@@ -22,16 +22,90 @@ Rows may only contain integers: 1..N (N included)
 Columns may only contain integers: 1..N (N included)
 'Little squares' (3x3 in example above) may also only contain integers: 1..N (N included)
 """
+
 class Sudoku(object):
     def __init__(self, data):
         self.sudokuList = data
 
     def validate_sudoku_size(self):
-        if is_integer(self.sudokuList**(1/2)):
-            pass
+        if not (len(self.sudokuList)**(1/2)).is_integer():
+            return False
+        for row in self.sudokuList:
+            if not (len(row)**(1/2)).is_integer():
+                return False
+        return True
 
-    def is_valid(self):
-        pass
+    def validate_integer_at_sudoku(self):
+        for row in self.sudokuList:
+            for cell in row:
+                print(cell)
+                if isinstance(cell, str) or isinstance(cell, bool) or cell > len(row) or cell < 1:
+                    return False
+            print()
+        return True
+    
+    @staticmethod
+    def validate_row(square, row: list):
+        if row.count(square) > 1:
+            return False
+        return True
+
+    @staticmethod
+    def validate_column(square, column: list):
+        if column.count(square) > 1:
+            return False
+        return True
+
+    @staticmethod
+    def validate_sector(square, sector: list):
+        if sector.count(square) > 1:
+            return False
+        return True
+
+    def validate_sudoku_rules(self):
+        for rowIndex, row in enumerate(self.sudokuList):
+            for index, square in enumerate(row):
+                raizQ = len(index)**(1/2)
+                sectorRow = (index//(raizQ))
+                
+                if not (self.validate_row(square, row) and self.validate_column(square, [rowComprehension[sectorRow*raizQ:(sectorRow+1)*raizQ] if rowIndexComprehension//raizQ == rowIndex//raizQ else None for rowIndexComprehension, rowComprehension in enumerate(self.sudokuList)])):
+                    return False
+        return True                                                                                                                                                                                                                                                                                         
+
+    def is_valid(self):                                                                                                                                                                                                                             
+        if self.sudokuList != [] and self.sudokuList != 0:
+            return (self.validate_integer_at_sudoku() and self.validate_sudoku_size() and self.validate_sector())
+        return False
+
+goodSudoku1 = Sudoku([
+  [1, 2 ,3],
+  [1, 2 ,3],
+  [1, 2 ,3],
+])
+
+print(goodSudoku1.is_valid())
 
 
-is_integer(7.2))
+"""
+[
+[1, 2, 3, 4, 5, 6, 7, 8, 9], 
+[2, 3, 1, 5, 6, 4, 8, 9, 7], 
+[3, 1, 2, 6, 4, 5, 9, 7, 8], 
+[4, 5, 6, 7, 8, 9, 1, 2, 3], 
+[5, 6, 4, 8, 9, 7, 2, 3, 1], 
+[6, 4, 5, 9, 7, 8, 3, 1, 2], 
+[7, 8, 9, 1, 2, 3, 4, 5, 6], 
+[8, 9, 7, 2, 3, 1, 5, 6, 4], 
+[9, 7, 8, 3, 1, 2, 6, 4, 5]
+]
+[
+[7, 8, 4, 1, 5, 9, 3, 2, 6] ,
+[5, 3, 9, 6, 7, 2, 8, 4, 1] ,
+[6, 1, 2, 4, 3, 8, 7, 5, 9] ,
+[9, 2, 8, 7, 1, 5, 4, 6, 3] ,
+[3, 5, 7, 8, 4, 6, 1, 9, 2] ,
+[4, 6, 1, 9, 2, 3, 5, 8, 7] ,
+[8, 7, 6, 3, 9, 4, 2, 1, 5] ,
+[2, 4, 3, 5, 6, 1, 9, 7, 8] ,
+[1, 9, 5, 2, 8, 7, 6, 3, 4] 
+]"""
