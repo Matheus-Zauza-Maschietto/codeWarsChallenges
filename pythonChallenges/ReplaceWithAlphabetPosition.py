d730c3c8dd558c628569a87c594142b0b1bf628e
matheus zauza maschietto <matheus_maschieto@yahoo.com.br>
2022-12-12 15:47:40 -0300

    update

--- pythonChallenges/ReplaceWithAlphabetPosition.py
+++ pythonChallenges/ReplaceWithAlphabetPosition.py
@@ -1,12 +1,8 @@
 def alphabet_position(text):
     alfabeto = 'abcdefghijklmnopqrstuvwxyz'
-    if text == ' ':
-        return ''
-    else:
-        alfanumerado = text
-        for c in range(10):
-            alfanumerado = alfanumerado.replace(str(c), " ")
-        alfanumerado = ' '.join([item.replace(item, str(alfabeto.find(item.lower())+1)) for item in alfanumerado.strip()])
-        return alfanumerado
+    for c in range(10):
+        text = text.replace(str(c), " ")
+    alfanumerado = ' '.join([item.replace(item, str(alfabeto.find(item.lower())+1)) for item in text.strip()])
+    return alfanumerado
     
 print("@@@"+alphabet_position('0 0 0 0')+"@@@")
