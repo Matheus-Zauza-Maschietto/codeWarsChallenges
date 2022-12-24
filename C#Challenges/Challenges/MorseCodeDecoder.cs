8ee9c379aa4ac791a16656cd4cdf0ac16c461756
Matheus Zauza Maschietto <matheus_maschietto@yahoo.com.br>
2022-12-24 13:49:09 -0300

    update desafio decoder codigo morse

--- C#Challenges/Challenges/MorseCodeDecoder.cs
+++ C#Challenges/Challenges/MorseCodeDecoder.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace C_Challenges.Challenges
+namespace C_Challenges.Challenges;
+
+    class MorseCodeDecoder
 {
-    public class MorseCodeDecoder
-    {
-        
-    }
+	public static string Decode(string morseCode)
+	{
+        Dictionary<string, string> dict = new Dictionary<string, string>(){{"", " "}, {".-", "A"}, {"-…", "B"}, {"-.-.", "C"}, {"-..", "D"},
+                                                                           {".", "E"}, {"..-.", "F"}, {"-.", "G"}, {"....", "H"}, {"..", "I"},
+                                                                           {".-", "J"}, {"-.-", "K"}, {".-..", "L"}, {"-", "M"}};
+        		List<string> strEnconded = new List<string>(morseCode.Split(" "));
+        foreach(var item in strEnconded){
+            System.Console.Write(item);
+        }
+        return "";
+	}
 }
