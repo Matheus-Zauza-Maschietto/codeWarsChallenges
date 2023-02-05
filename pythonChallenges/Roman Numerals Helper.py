d3fa10ce3e46105d2c737a16e0012381a284ac22
Matheus Zauza Maschietto <matheus_maschietto@yahoo.com.br>
2023-02-05 00:47:46 -0300

    Update Roman Numerals Helper.py

--- pythonChallenges/Roman Numerals Helper.py
+++ pythonChallenges/Roman Numerals Helper.py
@@ -22,9 +22,27 @@ D	500
 M	1000
 """
 class RomanNumerals:
-
     def to_roman(val):
         return ''
 
-    def from_roman(roman_num):
-        return 0
+    def from_roman( roman_num):
+        romanToNumeral = {
+            "M": 1000,
+            "D": 500,
+            "C": 100,
+            "L": 50,
+            "X": 10,
+            "V": 5,
+            "IV": 4,
+            "I": 1
+        }
+        sum = 0
+        for index in range(len(roman_num)):
+            if romanToNumeral[roman_num[index: index+1].upper()] == "IV":
+                sum += romanToNumeral[roman_num[index: index+1].upper()]
+            
+            sum += romanToNumeral[roman_num[index].upper()]
+        return sum
+
+
+print(RomanNumerals.from_roman("M"))
